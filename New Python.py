88e6e292ffd515b6ee09a9117df5a2503ffce880
Aram <aram2700@gmail.com>
2019-06-11 11:23:39 +0430

    initial commit

---
+++ New Python.py
@@ -0,0 +1,8 @@
+# Welcome to Python 
+import subprocess
+print("\n The owner is {}, The Laptop Brand is {}, {} "
+.format('Aram Mostowfi', 'Lenevo ThinkPad', 'E470'))
+print("Python Version is:")
+subprocess.call(["python", "-V"])
+###
+
